9b22990510d5b968db669838e0e5ebbdb7b28271
Gerardo <al07001821@tecmilenio.mx>
2025-02-16 21:12:48 -0600

    Libreria Jest y pipeline de CI/CD

--- server-1.js
+++ server-1.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const fs = require('fs').promises;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
